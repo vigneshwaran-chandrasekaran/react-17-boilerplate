ed1c350600ad05669a32d05bf375c1235abe7ec4
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-12-24 22:24:50 +0530

    added absolute path config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+
+reportWebVitals(console.log);
 
 ReactDOM.render(
   <React.StrictMode>
