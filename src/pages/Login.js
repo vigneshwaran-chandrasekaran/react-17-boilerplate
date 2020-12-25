8d6ba5366fb64c3889c99508160fb28cd3f5f8ac
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-12-25 23:19:58 +0530

    added basic grid to login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,17 @@
+import { Col, Row } from 'antd';
 import { LoginForm } from 'forms';
 import React from 'react';
 
 function Login(props) {
 	return (
-		<div>
-			<LoginForm />
-		</div>
+		<Row justify="space-around" align="middle">
+			<Col span={12}>
+				<LoginForm />
+			</Col>
+			<Col span={12}>
+				<LoginForm />
+			</Col>
+		</Row>
 	);
 }
 
