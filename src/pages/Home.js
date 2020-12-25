42dd11cb3591b299bd3ce849aa97c6e47780f1b0
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-12-25 16:58:19 +0530

    basic files and folders are added

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Home(props) {
+  return <div>Home</div>;
+}
+
+Home.propTypes = {};
+
+export default Home;
---
+++ src/pages/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Login(props) {
+  return <div>Login</div>;
+}
+
+Login.propTypes = {};
+
+export default Login;
