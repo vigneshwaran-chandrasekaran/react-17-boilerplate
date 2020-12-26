8f672e1608651ed030954ebfb1c322d0ae34caf3
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-12-26 12:22:30 +0530

    un auth pages common header added

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,10 @@
-import { Layout } from 'antd';
+import { Layout, Menu } from 'antd';
 import { AuthRoute, Loader, PrivateRoute } from 'components/common';
 import * as Pages from 'pages';
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 
-const { Content } = Layout;
+const { Header, Footer, Sider, Content } = Layout;
 
 export default function Router() {
 	return (
@@ -12,15 +12,46 @@ export default function Router() {
 			<BrowserRouter>
 				<Loader />
 				<Switch>
-					<Route exact path={['/home', '/']}>
-						<Pages.Home />
-					</Route>
-					<AuthRoute exact path="/login">
-						<Pages.Login />
-					</AuthRoute>
-					<AuthRoute exact path="/signup">
-						<Pages.Signup />
-					</AuthRoute>
+					{/* Unauth routes start here*/}
+					<Layout>
+						<Header className="header">
+							<Menu
+								theme="dark"
+								mode="horizontal"
+								defaultSelectedKeys={['2']}
+							>
+								<Menu.Item key="1">
+									<Link to="/home" className="link">
+										Home
+									</Link>
+								</Menu.Item>
+								<Menu.Item key="2">
+									<Link to="/signup" className="link">
+										Signup
+									</Link>
+								</Menu.Item>
+								<Menu.Item key="3">
+									<Link to="/login" className="link">
+										Login
+									</Link>
+								</Menu.Item>
+							</Menu>
+						</Header>
+						<Content>
+							<Switch>
+								<Route exact path={['/home', '/']}>
+									<Pages.Home />
+								</Route>
+								<AuthRoute exact path="/login">
+									<Pages.Login />
+								</AuthRoute>
+								<AuthRoute exact path="/signup">
+									<Pages.Signup />
+								</AuthRoute>
+							</Switch>
+						</Content>
+					</Layout>
+					{/* Unauth routes ends here*/}
 					<Layout style={{ minHeight: '100vh' }}>
 						<Layout>
 							<Content style={{ padding: '25px' }}>
