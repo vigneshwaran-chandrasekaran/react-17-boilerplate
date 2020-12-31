06973fc8b7aa5d8e912cb1271c7909516f310c34
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-12-31 23:04:17 +0530

    dynamic table value added

--- src/pages/TasksList.js
+++ src/pages/TasksList.js
@@ -1,7 +1,21 @@
 import { Table } from 'antd';
+import { labelOptions, priorityTypes, typeOptions } from 'helpers/enum';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getTasks } from 'store/taskSlice';
+
+function getLabel(id) {
+	return labelOptions.find((item) => item.value === id)?.label;
+}
+
+function getPriority(id) {
+	console.log('getPriority id', id);
+	return priorityTypes.find((item) => item.value === id)?.label;
+}
+
+function getType(id) {
+	return typeOptions.find((item) => item.value === id)?.label;
+}
 
 function TasksList() {
 	const dispatch = useDispatch();
@@ -23,15 +37,38 @@ function TasksList() {
 			key: 'description',
 		},
 		{
-			title: 'createdAt',
-			dataIndex: 'createdAt',
-			key: 'createdAt',
+			title: 'Type',
+			dataIndex: 'type', // to get data from api
+			key: 'type', // for column sorting key
+			render: (type) => getType(type) || 'NA',
+		},
+		{
+			title: 'Priority',
+			dataIndex: 'priority', // to get data from api
+			key: 'priority', // for column sorting key
+			render: (priority) => getPriority(priority) || 'NA',
+		},
+		{
+			title: 'Label',
+			dataIndex: 'priority', // to get data from api
+			key: 'priority', // for column sorting key
+			render: (priority) => getPriority(priority) || 'NA',
+		},
+		{
+			title: 'DueDate',
+			dataIndex: 'dueDate',
+			key: 'dueDate',
 		},
 	];
 
 	return (
 		<div>
-			<Table dataSource={tasks} columns={columns} />;
+			<Table
+				dataSource={tasks}
+				columns={columns}
+				rowKey={(record) => record?._id}
+			/>
+			;
 		</div>
 	);
 }
