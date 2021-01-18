1645cd7bd0845b401a420bd0c5602450c4644a15
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2021-01-18 11:44:49 +0530

    dashboard page testing file added

---
+++ src/__tests__/Dashboard.test.js
@@ -0,0 +1,13 @@
+import { screen } from '@testing-library/dom';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
+import Dashboard from 'pages/Dashboard';
+
+test('renders Dashboard', () => {
+	const { container, getByText } = render(<Dashboard />);
+	screen.debug();
+	expect(getByText('Dashboard')).toBeInTheDocument();
+	// 	expect(container.firstChild).toMatchInlineSnapshot(`
+	//     <h1>Dashboard</h1>
+	//   `);
+});
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Dashboard(props) {
 	console.log('Dashboard page');
-	return <div>Dashboard</div>;
+	return <h1>Dashboard</h1>;
 }
 
 Dashboard.propTypes = {};
